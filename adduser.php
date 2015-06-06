d8f616d597c8c142af561d550b347460f79851aa
hristo1 <hr_enchev@abv.bg>
2015-06-06 12:21:12 +0300

    Final

---
+++ adduser.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * Страница за вход
+ */
+ require 'includes/init.inc';
+
+	$page_title = 'Регистрация';
+	$navigation = ' / <a href="'.$_SERVER['PHP_SELF'].'">Регистрация</a>';
+
+	require 'includes/header.inc';
+?>
+
+<div align="center"> 
+
+
+</div>
+
+<?php require 'includes/footer.inc' ?>
